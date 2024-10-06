1584060d5587e461d0d59ac6578642a8b113472f
VidTu <imvidtu@proton.me>
2024-10-06 17:59:49 +0300

    build: Update a few build details

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 
 Mod that sometimes somehow block somewhat resembling clicking inventory by someone.
 
+## Dependencies
+
+**Fabric**: [Fabric API](https://modrinth.com/mod/fabric-api) (Required),
+[Cloth Config API](https://modrinth.com/mod/cloth-config) (Required),
+[Mod Menu](https://modrinth.com/mod/modmenu) (Optional)  
+**Quilt**: [QFAPI/QSL](https://modrinth.com/mod/qsl) (Required),
+[Cloth Config API](https://modrinth.com/mod/cloth-config) (Required),
+[Mod Menu](https://modrinth.com/mod/modmenu) (Optional)
+
 ## FAQ
 
-**Q**: Why?  
+**Q**: Why does this mod exist?  
 **A**: Because I've been [asked](https://i.imgur.com/5Nz1QRo.png) by a friend.
 
 **Q**: I need help, have questions, or something else.  
@@ -14,23 +23,30 @@ Mod that sometimes somehow block somewhat resembling clicking inventory by someo
 **A**: [GitHub](https://github.com/VidTu/BSCFSIO).
 
 **Q**: Which mod loaders are supported?  
-**A**: Fabric.
+**A**: Fabric is supported, Quilt should work too. Forge and NeoForge are not supported and never will be.
 
 **Q**: Which versions are supported?    
-**A**: Minecraft 1.20.1.
+**A**: Minecraft 1.20.1 only.
 
-**Q**: Why not Forge or 1.20.6 or 1.19.4 or 1.20.4?  
+**Q**: Why not Forge or 1.20.6 or 1.21.1 or 1.19.4 or 1.20.4?  
 **A**: Because my friend that asked my plays Fabric 1.20.1. Fork it and PR it, maybe I'll accept.
 
 **Q**: Do I need Fabric API?    
-**A**: Yes, you'll need Fabric API.
+**A**: Yes, you'll need Fabric API. You'll also need Cloth Config.
 
-**Q**: Is it open source?    
+**Q**: Is this mod open source?    
 **A**: Yes. (Licensed under [MIT](https://github.com/VidTu/BSCFSIO/blob/main/LICENSE))
 
-**Q**: I found a bug.  
+**Q**: I've found a bug.  
 **A**: Report it [here](https://github.com/VidTu/BSCFSIO/issues). There's no guarantee it'll be fixed.
 If you are not sure if this is a bug, you can join the [Discord](https://discord.gg/Q6saSVSuYQ).
+
+**Q**: Can I use this in my modpack?  
+**A**: Sure, but why? Credit (e.g. a link to mod's GitHub page) is appreciated, but is not required.
+Monetization and redistributing is allowed as per the [MIT License](https://github.com/VidTu/BSCFSIO/blob/main/LICENSE).
+
+**Q**: Why this mod is not on the Modrinth or CurseForge?  
+**A**: I don't think it's THAT useful to be there.
 
 **Q**: Isn't it a hack?  
 **A**: I don't think so and I don't care.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 VidTu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 plugins {
-    id("java")
-    alias(libs.plugins.fabric.loom)
+    alias(libs.plugins.architectury.loom)
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
@@ -8,12 +31,31 @@ java.targetCompatibility = JavaVersion.VERSION_17
 java.toolchain.languageVersion = JavaLanguageVersion.of(17)
 group = "ru.vidtu.bscfsio"
 base.archivesName = "BSCFSIO-Fabric-1.20.1"
+description = "Mod that sometimes somehow block somewhat resembling clicking inventory by someone."
 
 repositories {
     mavenCentral()
     maven("https://maven.fabricmc.net/")
     maven("https://maven.terraformersmc.com/releases/")
     maven("https://maven.shedaniel.me/")
+}
+
+loom {
+    silentMojangMappingsLicense()
+    runs.named("client") {
+        vmArgs(
+            "-XX:+IgnoreUnrecognizedVMOptions",
+            "-Xmx2G",
+            "-XX:+AllowEnhancedClassRedefinition",
+            "-XX:+AllowRedefinitionToAddDeleteMethods",
+            "-XX:HotswapAgent=fatjar",
+            "-Dfabric.debug.disableClassPathIsolation=true"
+        )
+    }
+    @Suppress("UnstableApiUsage")
+    mixin {
+        defaultRefmapName = "bscfsio.mixins.refmap.json"
+    }
 }
 
 dependencies {
@@ -24,8 +66,6 @@ dependencies {
     // Fabric (Provided)
     modImplementation(libs.fabric.loader)
     modImplementation(libs.fabric.api)
-
-    // Dependencies (Provided)
     modImplementation(libs.modmenu)
     modImplementation(libs.cloth.config)
 }
