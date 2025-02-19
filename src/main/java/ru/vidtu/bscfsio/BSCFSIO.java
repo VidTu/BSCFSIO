0e8e8c41e99edb8e40ca61271be64fd71b74efc3
VidTu <imvidtu@proton.me>
2025-02-19 06:13:26 +0300

    fix: Fix toggle bind not saving config

--- src/main/java/ru/vidtu/bscfsio/BSCFSIO.java
+++ src/main/java/ru/vidtu/bscfsio/BSCFSIO.java
@@ -24,6 +24,7 @@
 
 package ru.vidtu.bscfsio;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -94,6 +95,9 @@ public final class BSCFSIO implements ClientModInitializer {
 
             // Toggle.
             boolean newState = (BConfig.enabled = !BConfig.enabled);
+
+            // Save the config.
+            AutoConfig.getConfigHolder(BConfig.class).save();
 
             // Show the overlay and play the sound.
             client.gui.setOverlayMessage(Component.translatable("bscfsio." + newState)
